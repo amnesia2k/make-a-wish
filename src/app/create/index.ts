86f55dd6643dd40ff10fced2aa5f8c7f3c85a0c1
amnesia2k <tilewaolatoye17@gmail.com>
2025-03-31 22:40:12 +0100

    feat: add TypeScript declarations for image file types and update wishType images to use new paths

--- src/app/create/index.ts
+++ src/app/create/index.ts
@@ -14,69 +14,69 @@ export const wishType = [
     id: 1,
     name: "Happy Birthday",
     color: "oklch(88.03% 0.06 351.5)",
-    image: birthdayCard,
+    image: { src: "/card/Birthdaybg.webp" },
   },
 
   {
     id: 2,
     name: "Congratulations",
     color: "oklch(91.28% 0.1681 144.05)",
-    image: congratulationsCard,
+    image: { src: "/card/Congratulationsbg.webp" },
   },
 
   {
     id: 3,
     name: "Thank You",
     color: "oklch(60.93% 0.3034 312.94)",
-    image: thanksCard,
+    image: { src: "/card/Thanksbg.webp" },
   },
 
   {
     id: 4,
     name: "Mother's Day",
     color: "oklch(88.18% 0.1499 200.08)",
-    image: motherCard,
+    image: { src: "/card/mother.webp" },
   },
 
   {
     id: 5,
     name: "Love & Anniversaries",
     color: "oklch(81.35% 0.113 323.51)",
-    image: anniversaryCard,
+    image: { src: "/card/Anniversarybg.webp" },
   },
 
   {
     id: 6,
     name: "Sorry/Apologies",
     color: "oklch(87.51% 0.0502 205.73)",
-    image: sorryCard,
+    image: { src: "/card/Sorrybg.webp" },
   },
 
   {
     id: 7,
     name: "Just Because...",
     color: "oklch(82.12% 0.1089 88.87)",
-    image: justBecauseCard,
+    image: { src: "/card/JBbg.webp" },
   },
 
   {
     id: 8,
     name: "Father's Day",
     color: "oklch(96.36% 0.0328 107)",
-    image: fatherCard,
+    image: { src: "/card/father.webp" },
   },
 
   {
     id: 9,
     name: "Valentine's",
     color: "oklch(74.11% 0.2001 345.28)",
-    image: valentineCard,
+    image: { src: "/card/vall.webp" },
   },
 
   {
     id: 10,
     name: "Christmas & New Year's",
     color: "oklch(96.8% 0.211 109.77)",
-    image: christmasCard,
+    image: { src: "/card/Christmasbg.webp" },
   },
 ];
